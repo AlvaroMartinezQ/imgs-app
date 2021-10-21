8f510128c203893413d662a7e8b89d3d50d57e04
AlvaroMartinezQ <alvaro.martinez-quiroga@partners.basf.com>
2021-10-21 12:28:24 +0200

    Migrated to VueCLI

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:AlvaroMartinezQ/nasa-app.git master:gh-pages
+git push -f git@github.com:AlvaroMartinezQ/imgs-app.git master:gh-pages
 
 cd -
